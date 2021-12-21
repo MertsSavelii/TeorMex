94060dd889316b28012a721d5d28edb63b11b05a
MertsSavelii <mokrakrasawa1@gmail.com>
2021-12-21 15:04:39 +0300

    w

--- lab3/main.py
+++ lab3/main.py
@@ -5,7 +5,6 @@ from scipy.integrate import odeint
 import sympy as sp
 import math
 
-
 def formY(y, t, fV, fOm):
     y1,y2,y3,y4 = y
     dydt = [y3,y4,fV(y1,y2,y3,y4),fOm(y1,y2,y3,y4)]
@@ -40,7 +39,7 @@ Vr2 = V1**2 + V2**2
 TT2 = m * Vr2 / 2
 TT = TT1+TT2
 # 2 defining the potential energy
-Pi1 = m * g *  sp.cos(psi) * l
+Pi1 = 2 * R * m * g * sp.sin(psi)**2
 Pi2 = (c * (l - l0)**2) / 2
 Pi = Pi1+Pi2
 # 3 Not potential force
@@ -50,8 +49,8 @@ M = alpha * phi**2;
 L = TT-Pi
 
 # equations
-ur1 = ((M / 2) + m * sp.sin(2 * psi)**2) * R**2 * sp.diff(Vphi) + 2 * m * R**2 * Vphi*Vpsi*sp.sin(4*psi) - alpha * phi**2
-ur2 = m * R * (4 * sp.diff(Vpsi) - Vphi**2 * sp.sin(4*phi)) + 2 * m * g * sp.sin(2*psi) - 2*c*(2*R*sp.cos(psi) - l0)*sp.sin(psi)
+ur1 = sp.diff(sp.diff(L,Vphi),t)-sp.diff(L,phi) - M
+ur2 = sp.diff(sp.diff(L,Vpsi),t)-sp.diff(L,psi)
 
 # isolating second derivatives(dV/dt and dom/dt) using Kramer's method
 a11 = ur1.coeff(sp.diff(Vphi,t),1)
@@ -71,12 +70,12 @@ domdt = detA2/detA
 countOfFrames = 2000
 
 # Constructing the system of differential equations
-T = np.linspace(0, 12, countOfFrames)
+T = np.linspace(0, 25, countOfFrames)
 # Pay attention here, the function lambdify translate function from the sympy to numpy and then form arrays much more
 # faster then we did using subs in previous lessons!
 fVphi = sp.lambdify([phi,psi,Vphi,Vpsi], dVdt, "numpy")
 fVpsi = sp.lambdify([phi,psi,Vphi,Vpsi], domdt, "numpy")
-y0 = [20, -1, -25, 0]
+y0 = [0, np.pi/6, -0.5, 0]
 sol = odeint(formY, y0, T, args = (fVphi, fVpsi))
 
 #sol - our solution
@@ -85,53 +84,39 @@ sol = odeint(formY, y0, T, args = (fVphi, fVpsi))
 #sol[:,2] - dphi/dt
 #sol[:,3] - dpsi/dt
 
-print(sol[:,0])
-
 # Ввод переменной t и радиусов необходимых окружностей + ввод угла поворота шариков
 t = sp.Symbol('t')
-R = 2
 
 # Построение графика и подграфика с выравниванием осей
 fig = plt.figure(figsize=(17, 8))
 ax1 = fig.add_subplot(1, 2, 1)
 ax1.axis('equal')
 
-# phi = np.linspace(0, 2 * math.pi, 500)
-# psi = np.linspace(-math.pi/2, 0, 500)
-
 phi = sol[:,0]
 psi = sol[:,1]
-w = np.linspace(0, 2 * math.pi, 2000)
-conline, = ax1.plot([sp.sin(2*psi[0]) * R * np.abs(sp.cos(phi[0])), 0], [-1, R], 'black')
-P, = ax1.plot(sp.sin(2*psi[0]) * R * np.abs(sp.cos(phi[0])), sp.cos(2*psi[0]) * R, marker='o', color='black')
-Circ, = ax1.plot(R * np.abs(sp.cos(phi[0])) * np.cos(w), R * np.sin(w), 'black')
+Vphi = sol[:,2]
+Vpsi = sol[:,3]
+
+w = np.linspace(0, 2 * math.pi, countOfFrames)
+conline, = ax1.plot([sp.sin(2*psi[0]) * R * sp.cos(phi[0]), 0], [-1, R], 'black')
+P, = ax1.plot(sp.sin(2*psi[0]) * R * sp.cos(phi[0]), sp.cos(2*psi[0]) * R, marker='o', color='black')
+Circ, = ax1.plot(R * sp.cos(phi[0]) * np.cos(w), R * np.sin(w), 'black')
 
 #Доп графики
 ax2 = fig.add_subplot(4, 2, 2)
-T = np.linspace(0, 2 * math.pi, 2000)
-x = sp.sin(t)+2
-y = sp.cos(t+math.pi)+2
-Vx = sp.diff(x, t)
-Vy = sp.diff(y,t)
-T = np.linspace(0, 10, 2000)
-VX = np.zeros_like(T)
-VY = np.zeros_like(T)
-for i in np.arange(len(T)):
-    VX[i] = sp.Subs(Vx, t, T[i])
-    VY[i] = sp.Subs(Vy, t, T[i])
-ax2.plot(T, VX)
+ax2.plot(T, Vphi)
 ax2.set_xlabel('T')
-ax2.set_ylabel('VX')
+ax2.set_ylabel('Vphi')
 ax3 = fig.add_subplot(4, 2, 4)
-ax3.plot(T, VY)
+ax3.plot(T, Vpsi)
 ax3.set_xlabel('T')
-ax3.set_ylabel('VY')
+ax3.set_ylabel('Vpsi')
 
 def anima(i):
-    P.set_data(sp.sin(2*psi[i]) * R * np.abs(sp.cos(phi[i])), sp.cos(2*psi[i]) * R)
-    conline.set_data([sp.sin(2*psi[i]) * R * np.abs(sp.cos(phi[i])), 0], [sp.cos(2*psi[i]) * R, R])
-    Circ.set_data(R * np.abs(sp.cos(phi[i])) * np.cos(w), R * np.sin(w))
+    P.set_data(sp.sin(2*psi[i]) * R * sp.cos(phi[i]), sp.cos(2*psi[i]) * R)
+    conline.set_data([sp.sin(2*psi[i]) * R * sp.cos(phi[i]), 0], [sp.cos(2*psi[i]) * R, R])
+    Circ.set_data(R * sp.cos(phi[i]) * np.cos(w), R * np.sin(w))
     return Circ, P, conline
 
-anim = FuncAnimation(fig, anima, frames=2000, interval=35, blit=True)
+anim = FuncAnimation(fig, anima, frames=countOfFrames, interval=1, blit=True)
 plt.show()
